7c86b2b146bed7e546c2c1fea0bca69c43a7aa07
FaithBook233 <1203339302@qq.com>
2020-12-26 23:42:52 +0800

    预览窗口代码

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -29,13 +29,37 @@
 
 
 #include <iostream>
+using namespace std;
 
+#include <opencv.hpp>
+#include <highgui.hpp>
+using namespace cv;
 int main()
 {
 	int nx = 200;//图片宽度（单位为像素）
 	int ny = 100;//图片宽度（单位为像素）
+
 	//文件头写入
 	std::cout << "P3" << std::endl << nx << " " << ny << std::endl << "255" << std::endl;
+
+	//预览窗口相关
+	int WindowWidth;//窗口宽度
+	int WindowHeight;//窗口高度
+	if (nx > ny)  //如果渲染的图片为横版，计算窗口宽度与高度
+	{
+		WindowHeight = 1080 - 200;
+		WindowWidth = (int)((double)WindowHeight * (double)nx / (double)ny);
+	}
+	else //如果渲染的图片为竖版，计算窗口宽度与高度
+	{
+		WindowWidth = 1920 - 200;
+		WindowHeight = (int)((double)WindowWidth * (double)ny / (double)nx);
+
+	}
+	Mat lena(ny, nx, CV_8UC3, Scalar(50, 50, 50)); //创建一张图片
+	namedWindow("图像预览（渲染中）", WINDOW_NORMAL);//设置标题
+	moveWindow("图像预览（渲染中）", (int)((1920.0 - WindowWidth) / 2), (int)((1080.0 - WindowHeight) / 2) - 50);//设置窗口位置
+	resizeWindow("图像预览（渲染中）", WindowWidth, WindowHeight);//设置窗口大小
 
 	for (int j = ny - 1; j >= 0; j--)//行信息
 	{
@@ -53,6 +77,13 @@ int main()
 			int ib = int(255.99 * b);
 			//将当前像素的三个通道值写入文件
 			std::cout << ir << " " << ig << " " << ib << std::endl;
+
+			lena.at<cv::Vec3b>(ny - 1 - j, i)[0] = ib;
+			lena.at<cv::Vec3b>(ny - 1 - j, i)[1] = ig;
+			lena.at<cv::Vec3b>(ny - 1 - j, i)[2] = ir;
 		}
+		//每行计算完以后刷新预览窗口
+		imshow("图像预览（渲染中）", lena);
+		waitKey(1);
 	}
 }
