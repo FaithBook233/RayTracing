a54ee585eb0cf542563d538840870298ea5d5f30
root <1203339302@qq.com>
2022-05-26 08:58:02 +0800

    tag:一周实现光线追踪（二）opencv窗口实时显示渲染结果

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 搭建基本环境，输出一个简单渐变图片到RayTracing目录下的Image.ppm
 
-克隆仓库后，直接将opencv.rar解压到当前文件夹
+**克隆仓库后，直接将opencv.rar解压到当前文件夹**
+**然后将opencv\build\vc15\bin的绝对路径添加到系统变量的Path中**
--- RayTracing.cpp
+++ RayTracing.cpp
@@ -1,29 +1,91 @@
-﻿
-#include <iostream>
+﻿/*
+  * 作者（B站UP主，感谢三连！！！）：FaithBook-_-
+  * GitHub：FaithBook233
+  * 技术交流群（非粉丝群）：815093873
+  *
+  *
+  * 本项目是我学习C++与渲染算法的一个小Demo，代码开源供广大网友学习
+  * 有问题尽量自己解决，知识不够先问度娘，加油少年！
+  *
+  * 运行环境：Windows 10 Visual Studio 2019
+  * 代码参考《Ray Tracing in One Weekend》
+  * 文献中并未提及运行环境等信息，我见到有人用Visual Studio Code实现了，但我更热衷于用VS，所以决定用Visual Studio实现代码
+  * 文献中代码为偏C风格，我将代码改为纯C++代码，并对算法和代码命名风格进行了升级与整理
+  * 原文为单精度浮点，我整体改为双精度浮点了
+  *
+  * 运行此项目必须的步骤：
+  *       ①准备Visual Studio 2019或更高的版本，低版本可能有兼容问题
+  *       ②（已实现）在项目属性->调试->命令行调试中输入 >Image.ppm  可以将控制台程序输出到Image.ppm
+  *       ③用ppm viewer打开Image.ppm即可看到效果
+  *
+  * 参考文献《Ray Tracing in One Weekend》 Peter Shirley Version 1.54 Copyright 2018. Peter Shirley. All rights reserved
 
+*/
+
+
+
+#include <iostream>
+#include <opencv.hpp>
+#include <highgui.hpp>
+using namespace cv;
 int main()
 {
-    int nx = 200;//图片宽度（单位为像素）
-    int ny = 100;//图片宽度（单位为像素）
-    //文件头写入
-    std::cout << "P3" << std::endl << nx << " " << ny << std::endl << "255" << std::endl;
-    
-    for (int j = ny - 1; j >= 0; j--)//行信息
-    {
-        for (int i = 0; i < nx; i++)//列信息
-        {
-            //注意原书上的类型为float，以后的代码中会整体采用double
-            //计算每个像素上的RGB信息
-            double r = double(i) / double(nx);
-            double g = double(j) / double(ny);
-            //每个像素的蓝色通道固定为0.2
-            double b = 0.2;
-            //因为ppm文件中RGB通道值为0-255，而上面计算出来的r,g,b范围为0-1，这里进行转换
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-            //将当前像素的三个通道值写入文件
-            std::cout << ir << " " << ig << " " << ib << std::endl;
-        }
-    }
+	int nx = 200;//图片宽度（单位为像素）
+	int ny = 100;//图片宽度（单位为像素）
+
+	//文件头写入
+	std::cout << "P3" << std::endl << nx << " " << ny << std::endl << "255" << std::endl;
+
+	//预览窗口相关
+	int WindowWidth;//窗口宽度
+	int WindowHeight;//窗口高度
+	if (nx > ny)  //如果渲染的图片为横版，计算窗口宽度与高度
+	{
+		WindowHeight = 1080 - 200;
+		WindowWidth = (int)((double)WindowHeight * (double)nx / (double)ny);
+	}
+	else //如果渲染的图片为竖版，计算窗口宽度与高度
+	{
+		WindowWidth = 1920 - 200;
+		WindowHeight = (int)((double)WindowWidth * (double)ny / (double)nx);
+
+	}
+	Mat RenderingImage(ny, nx, CV_8UC3, Scalar(50, 50, 50)); //创建一张图片
+	namedWindow("图像预览（渲染中）", WINDOW_NORMAL);//设置标题
+	moveWindow("图像预览（渲染中）", (int)((1920.0 - WindowWidth) / 2), (int)((1080.0 - WindowHeight) / 2) - 50);//设置窗口位置
+	resizeWindow("图像预览（渲染中）", WindowWidth, WindowHeight);//设置窗口大小
+
+	for (int j = ny - 1; j >= 0; j--)//行信息
+	{
+		for (int i = 0; i < nx; i++)//列信息
+		{
+			//注意原书上的类型为float，以后的代码中会整体采用double
+			//计算每个像素上的RGB信息
+			double r = double(i) / double(nx);
+			double g = double(j) / double(ny);
+			//每个像素的蓝色通道固定为0.2
+			double b = 0.2;
+			//因为ppm文件中RGB通道值为0-255，而上面计算出来的r,g,b范围为0-1，这里进行转换
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			//将当前像素的三个通道值写入文件
+			std::cout << ir << " " << ig << " " << ib << std::endl;
+
+			RenderingImage.at<cv::Vec3b>(ny - 1 - j, i)[0] = ib;
+			RenderingImage.at<cv::Vec3b>(ny - 1 - j, i)[1] = ig;
+			RenderingImage.at<cv::Vec3b>(ny - 1 - j, i)[2] = ir;
+		}
+		//每行计算完以后刷新预览窗口
+		if (!(j%(ny/100))) //每渲染ny/100行后更新预览窗口图片
+		{
+			imshow("图像预览（渲染中）", RenderingImage);
+			waitKey(1);//等待1毫秒事件让窗口刷新完毕
+		}
+		
+	}
+	imshow("图像预览（渲染中）", RenderingImage);
+	waitKey(3000); //等待3000毫秒
+	destroyAllWindows();//关闭窗口
+	return 0;
 }
